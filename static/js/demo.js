8113c09402dd5185f184c54b07c889116e301742
Michael Tanczos <mike@logic-gate.com>
2022-01-18 23:03:37 -0500

    Switched to dist tabler js

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,98 +1,151 @@
-// Setting items
-const items = {
-	'theme': { localStorage: 'tablerTheme', default: 'light' },
-	'menu-position': { localStorage: 'tablerMenuPosition', default: 'top' },
-	'menu-behavior': { localStorage: 'tablerMenuBehavior', default: 'sticky' },
-	'container-layout': { localStorage: 'tablerContainerLayout', default: 'boxed' },
-}
+/*!
+* Tabler v1.0.0-beta5 (https://tabler.io)
+* @version 1.0.0-beta5
+* @link https://tabler.io
+* Copyright 2018-2021 The Tabler Authors
+* Copyright 2018-2021 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+*/
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+})((function () { 'use strict';
 
-// Theme config
-const config = {}
-for (const [key, params] of Object.entries(items)) {
-	config[key] = localStorage.getItem(params.localStorage) ? localStorage.getItem(params.localStorage) : params.default
-}
+  function _slicedToArray(arr, i) {
+    return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+  }
 
-// Parse url params
-const parseUrl = () => {
-	const search = window.location.search.substring(1)
-	const params = search.split('&')
+  function _arrayWithHoles(arr) {
+    if (Array.isArray(arr)) return arr;
+  }
 
-	for (let i = 0; i < params.length; i++) {
-		const arr = params[i].split('=')
-		const key = arr[0]
-		const value = arr[1]
+  function _iterableToArrayLimit(arr, i) {
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
 
-		if (!!items[key]) {
-			// Save to localStorage
-			localStorage.setItem(items[key].localStorage, value)
+    if (_i == null) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
 
-			// Update local variables
-			config[key] = value
-		}
-	}
-}
+    var _s, _e;
 
-// Toggle form controls
-const toggleFormControls = (form) => {
-	for (const [key, params] of Object.entries(items)) {
-		const elem = form.querySelector(`[name="settings-${key}"][value="${config[key]}"]`)
+    try {
+      for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
 
-		if (elem) {
-			elem.checked = true
-		}
-	}
-}
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"] != null) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
 
-// Update body classes
-const updateBodyClasses = () => {
-	document.body.classList.remove('theme-dark');
-	document.body.classList.remove('theme-light');
+    return _arr;
+  }
 
-	document.body.classList.add(`theme-${config.theme}`);
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
 
-	// for (const [key, params] of Object.entries(items)) {
-	// 	document.body.setAttribute(`data-${key}`, config[key]);
-	// }
-}
+  function _arrayLikeToArray(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
 
-// Submit form
-const submitForm = (form) => {
-	// Save data to localStorage
-	for (const [key, params] of Object.entries(items)) {
-		// Save to localStorage
-		const value = form.querySelector(`[name="settings-${key}"]:checked`).value
-		localStorage.setItem(params.localStorage, value)
+    for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
 
-		// Update local variables
-		config[key] = value
-	}
+    return arr2;
+  }
 
-	// Update body classes
-	updateBodyClasses();
+  function _nonIterableRest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
 
-	window.dispatchEvent(new Event('resize'));
+  var items = {
+    'theme': {
+      localStorage: 'tablerTheme',
+      default: 'light'
+    },
+    'menu-position': {
+      localStorage: 'tablerMenuPosition',
+      default: 'top'
+    },
+    'menu-behavior': {
+      localStorage: 'tablerMenuBehavior',
+      default: 'sticky'
+    },
+    'container-layout': {
+      localStorage: 'tablerContainerLayout',
+      default: 'boxed'
+    }
+  };
+  var config = {};
+  for (var _i = 0, _Object$entries = Object.entries(items); _i < _Object$entries.length; _i++) {
+    var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+        key = _Object$entries$_i[0],
+        params = _Object$entries$_i[1];
+    config[key] = localStorage.getItem(params.localStorage) ? localStorage.getItem(params.localStorage) : params.default;
+  }
+  var parseUrl = function parseUrl() {
+    var search = window.location.search.substring(1);
+    var params = search.split('&');
+    for (var i = 0; i < params.length; i++) {
+      var arr = params[i].split('=');
+      var _key = arr[0];
+      var value = arr[1];
+      if (!!items[_key]) {
+        localStorage.setItem(items[_key].localStorage, value);
+        config[_key] = value;
+      }
+    }
+  };
+  var toggleFormControls = function toggleFormControls(form) {
+    for (var _i2 = 0, _Object$entries2 = Object.entries(items); _i2 < _Object$entries2.length; _i2++) {
+      var _Object$entries2$_i = _slicedToArray(_Object$entries2[_i2], 2),
+          _key2 = _Object$entries2$_i[0];
+          _Object$entries2$_i[1];
+      var elem = form.querySelector("[name=\"settings-".concat(_key2, "\"][value=\"").concat(config[_key2], "\"]"));
+      if (elem) {
+        elem.checked = true;
+      }
+    }
+  };
+  var updateBodyClasses = function updateBodyClasses() {
+    document.body.classList.remove('theme-dark');
+    document.body.classList.remove('theme-light');
+    document.body.classList.add("theme-".concat(config.theme));
+  };
+  var submitForm = function submitForm(form) {
+    for (var _i3 = 0, _Object$entries3 = Object.entries(items); _i3 < _Object$entries3.length; _i3++) {
+      var _Object$entries3$_i = _slicedToArray(_Object$entries3[_i3], 2),
+          _key3 = _Object$entries3$_i[0],
+          _params2 = _Object$entries3$_i[1];
+      var value = form.querySelector("[name=\"settings-".concat(_key3, "\"]:checked")).value;
+      localStorage.setItem(_params2.localStorage, value);
+      config[_key3] = value;
+    }
+    updateBodyClasses();
+    window.dispatchEvent(new Event('resize'));
+    new bootstrap.Offcanvas(form).hide();
+  };
+  parseUrl();
+  updateBodyClasses();
+  var form = document.querySelector('#offcanvasSettings');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      submitForm(form);
+    });
+    toggleFormControls(form);
+  }
 
-	(new bootstrap.Offcanvas(form)).hide()
-}
-
-
-// Parse url
-parseUrl()
-
-// Update body classes
-updateBodyClasses();
-
-// Elements
-const form = document.querySelector('#offcanvasSettings')
-
-// Toggle form controls
-if (form) {
-
-	form.addEventListener('submit', function (e) {
-		e.preventDefault()
-
-		submitForm(form)
-	})
-
-	toggleFormControls(form)
-}
+}));
